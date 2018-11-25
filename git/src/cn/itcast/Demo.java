1579cc3ada14e5209371ebc6df95cb35056749ac
lisi <lisi>
2018-11-25 20:27:20 +0800

    111

---
+++ git/src/cn/itcast/Demo.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("qwqwe");
+        System.out.println("qwqwe");
+        System.out.println("qwqwe");
+        System.out.println("qwqw");
+
+
+    }
+}
